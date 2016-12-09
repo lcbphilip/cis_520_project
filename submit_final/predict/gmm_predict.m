fa69e380a5a7a62278fcc21154c239ddae924773
Philip Changbo Li <lcbphilip@gmail.com>
2016-12-08 23:00:24 -0500

    whatever

--- submit_final/predict/gmm_predict.m
+++ submit_final/predict/gmm_predict.m
@@ -1,10 +1,23 @@
-function test_labels = gmm_predict(GMMmodels, priors, X_test)
+function test_labels = gmm_predict(X_test)
+
+gmm_models = load('models/gmm_models.mat');
+gmm_models = gmm_models.gmm_models;
+gmm_priors = load('models/gmm_priors.mat');
+gmm_priors = gmm_priors.gmm_priors;
+
+X_full_mean = load('models/X_full_mean.mat');
+X_full_mean = X_full_mean.X_full_mean;
+X_full_PCA_coeff = load('models/X_full_PCA_coeff.mat');
+X_full_PCA_coeff = X_full_PCA_coeff.X_full_PCA_coeff;
 
 test_sample_size = size(X_test, 1);
 posteriors = zeros(test_sample_size, 2);
 
+X_test_centered = X_test - repmat(X_full_mean, [test_sample_size, 1]);
+X_test_score = X_test_centered * X_full_PCA_coeff;
+
 for l=1:2
-    posteriors(:,l) = pdf(GMMmodels{l}, X_test) .* priors(l,1);
+    posteriors(:,l) = pdf(gmm_models{l}, X_test_score) .* gmm_priors(l,1);
 end
        
 [~, test_labels] = max(posteriors, [], 2);
--- submit_final/train/gmm_train.m
+++ submit_final/train/gmm_train.m
@@ -1,22 +1,32 @@
-function [gmm_model, priors] = gmm_train(X_train, Y_train)
+function [gmm_models, gmm_priors, X_full_PCA_coeff, X_full_mean] = gmm_train(X_train, Y_train, X_unlabeled)
 
 options = statset('MaxIter',500);
-num_clusters = 10;
+num_clusters = 100;
+num_pc = 200;
 
-GMMmodels = cell(2);
+X_full = [X_train; X_unlabeled];
+
+X_full_mean = mean(X_full);
+X_full_centered = X_full - repmat(X_full_mean, [size(X_full, 1), 1]);
+X_train_centered = X_train - repmat(X_full_mean, [size(X_train, 1), 1]);
+
+X_full_PCA_coeff = pca(X_full_centered, 'NumComponents', num_pc);
+
+X_train_score = X_train_centered * X_full_PCA_coeff;
+
+gmm_models = cell(2);
 
 %%
 
+gmm_priors = zeros(2,1);
 for l=1:2
-    priors(l,1) = size(Y_train(Y_train==l-1),1);
+    gmm_priors(l,1) = size(Y_train(Y_train==l-1),1);
 end
-priors = priors / sum(priors);
+gmm_priors = gmm_priors / sum(gmm_priors);
 
 for l=1:2
-    GMMmodels{l} = fitgmdist(X_train(Y_train==l-1,:),num_clusters, ...
+    gmm_models{l} = fitgmdist(X_train_score(Y_train==l-1,:),num_clusters, ...
     'CovarianceType','diagonal','RegularizationValue',0.1^5,'Options',options);
 end
-
-gmm_model = GMMmodels;
 
 end
